9ae3a0fd878b4fa86334eccf5953c5ee37a2d55e
ECEsquivel1433 <edgarcoronaesq@gmail.com>
2023-05-26 12:41:16 -0600

    Add project files.

---
+++ BL/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+
+        public string Direccion { get; set; }
+        public string Genero { get; set; }
+        public string Foto { get; set; }
+
+    }
+}
